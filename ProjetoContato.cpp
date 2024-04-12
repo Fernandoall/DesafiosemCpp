c696524b720a9bdaccacb7c49c4768dc77167570
Fernando Gomes <fernando.macena06@gmail.com>
2024-04-11 21:18:34 -0300

    Implementando função para cadastro de novos usuários

--- ProjetoContato.cpp
+++ ProjetoContato.cpp
@@ -43,4 +43,25 @@ public:
     }
 };
 
+// Função para cadastrar um novo contato
+Contato cadastrar_contato() {
+    string email, nome, telefone;
+    int dia, mes, ano;
+    cout << "Digite o e-mail do contato: ";
+    cin >> email;
+    cout << "Digite o nome do contato: ";
+    cin >> nome;
+    cout << "Digite o telefone do contato: ";
+    cin >> telefone;
+    cout << "Digite o dia de nascimento do contato: ";
+    cin >> dia;
+    cout << "Digite o mês de nascimento do contato: ";
+    cin >> mes;
+    cout << "Digite o ano de nascimento do contato: ";
+    cin >> ano;
+    Data dtnasc(dia, mes, ano);
+    Contato novo_contato(email, nome, telefone, dtnasc);
+    return novo_contato;
+}
+
 
