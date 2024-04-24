bff2348a036bda8e6e62ea95a0c915acf9f72044
Fernando Gomes <fernando.macena06@gmail.com>
2024-04-24 13:21:20 -0300

    Setando função para incluir funcionario a lista

--- Lista/Exercicio.cpp
+++ Lista/Exercicio.cpp
@@ -7,3 +7,25 @@ struct Funcionario {
     std::string nome;
     double salario;
 };
+
+void incluirFuncionario(std::vector<Funcionario>& funcionarios) {
+    Funcionario novoFuncionario;
+    std::cout << "Digite o prontuario: ";
+    std::cin >> novoFuncionario.prontuario;
+    std::cout << "Digite o nome: ";
+    std::cin.ignore(); // Limpar o buffer do teclado
+    std::getline(std::cin, novoFuncionario.nome);
+    std::cout << "Digite o salario: ";
+    std::cin >> novoFuncionario.salario;
+
+    // Verificar se já existe um funcionário com o mesmo prontuário
+    for (const auto& funcionario : funcionarios) {
+        if (funcionario.prontuario == novoFuncionario.prontuario) {
+            std::cout << "Já existe um funcionário com o mesmo prontuário." << std::endl;
+            return;
+        }
+    }
+
+    funcionarios.push_back(novoFuncionario);
+    std::cout << "Funcionário incluído com sucesso." << std::endl;
+}
